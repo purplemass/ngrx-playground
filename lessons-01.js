9a44b97fd0285d7077081f4f022518f3b70330e9
purplemass <bob@purplemass.com>
2016-06-24 01:08:09 +0100

    lesson01 -> throttle & delay

---
+++ lessons-01.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// ----------------------------------------------------------------------------
+
+var button = document.querySelector('.refresh');
+var label = document.querySelector('h5');
+
+// ----------------------------------------------------------------------------
+
+var clickStream = Rx.Observable.fromEvent(button, 'click')
+  .map(ev => ev.clientX); // <-- doesn't contribute much
+
+var doubleClickStream = clickStream
+  // .buffer(() => clickStream.throttle(250)) // <-- wrong in the lesson
+  .buffer(clickStream.throttle(250))          // <-- works now
+  .map(arr => arr.length)
+  .filter(len => len === 2);
+
+doubleClickStream.subscribe(x => {
+  label.textContent = 'Double clicked!';
+})
+
+doubleClickStream
+  // .throttle(1000)  // <-- wrong in the lesson
+  .delay(1000)        // <-- works now
+  .subscribe(x => {
+    label.textContent = '&nbsp;';
+  })
+
+/*
+
+// ----------------------------------------------------------------------------
+
+var slowStream = clickStream
+  .delay(2000)
+  .map(x => x/2)
+  ;
+
+slowStream.subscribe(x => {
+  console.info(`slow: ${x}`);
+  label.textContent = 'bad clear';
+})
+
+var source1 = Rx.Observable.interval(400).take(6)
+  .map(i => ['4', '33', 'e', 'a', 'd', 'p'][i]);
+source1.subscribe(x => console.log(x));
+
+var source2 = Rx.Observable.just(10);
+source2.subscribe(x => console.log(x));
+
+// ----------------------------------------------------------------------------
+
+*/
