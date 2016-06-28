11dd556df895cceb53ada7354a994be5a89bac32
purplemass <bob@purplemass.com>
2016-06-28 17:04:35 +0100

    using dataStream with no refresh user button

--- lesson-02.js
+++ lesson-02.js
@@ -85,11 +85,11 @@ function doRx() {
   // dropdown stream
   const dropDownChangeStream = Rx.Observable.fromEvent($input, 'change')
     .map(ev => parseInt(ev.target.value))
-    .startWith(1)
+    .startWith(3)
     .throttle(250);
 
-  // actions stream
-  const actionsStream = dropDownChangeStream
+  // data stream
+  const dataStream = dropDownChangeStream
     .combineLatest(
       responseStream, (dropdown, listUsers) => {
         return {
@@ -97,30 +97,32 @@ function doRx() {
           dropdown: dropdown
         };
     })
-    // .do(x => console.info('MAIN1:', x))
-    .flatMap(
-      result => {
-        return Rx.Observable.merge(
-          // userObservable(0, result.users)
-          userObservable(0, result.users),
-          userObservable(1, result.users),
-          userObservable(2, result.users),
-          userObservable(3, result.users)
-        )
-        // return userObservable(1, result.users);
-        // return [...Array(result.dropdown).keys()].map(n => {
-        //   userObservable(n, result.users);
-        // });
-      }
-    )
-    // .do(x => console.info('MAIN2:', x))
+    .do(x => console.info('MAIN1:', x))
+    .do(x => {
+      [...Array(apiAmount).keys()].forEach(inc => htmlUser(inc, null));
+    })
+    .filter(x => x.users.length)
+    .map(result => {
+      var ret = []
+      const temp = Rx.Observable
+        .range(1, result.dropdown)
+        .map(x => {
+          const listUsers = result.users;
+          const randomUser = listUsers[Math.floor(Math.random()*listUsers.length)];
+          ret.push(randomUser);
+        })
+        .subscribe()
+      temp.dispose();
+      return ret;
+    })
+    .do(x => console.info('MAIN2:', x))
 
-  actionsStream
-    .subscribe(user => {
-      // console.log('main', user);
-      if (user) {
-        htmlUser(user.x, user.user);
-      }
+  dataStream
+    .filter(x => !!x)
+    .subscribe(users => {
+      users.forEach((user, inc) => {
+        htmlUser(inc, user);
+      });
     });
 }
 
